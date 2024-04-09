1b9c31af7bdb9c1b58b28b7c1eb46c47e84f17ee
Mihail Solovev <solovei.0807@gmail.com>
2024-04-09 21:41:15 +0300

    add tsks app

--- project_tracker/quality_control/models.py
+++ project_tracker/quality_control/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+import tasks
 
-# Create your models here.
+class BugReport(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    project = models.ForeignKey(
+        tasks.Project,
+        related_name='tasks',
+        on_delete=models.CASCADE
+    ) 
